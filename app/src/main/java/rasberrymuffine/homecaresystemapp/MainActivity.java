e7d98c073d534e2ea1ee8a047d944ea93db4940c
sehyeon <sehyun_s@naver.com>
2015-09-10 12:59:55 +0900

    push test

--- app/src/main/java/rasberrymuffine/homecaresystemapp/MainActivity.java
+++ app/src/main/java/rasberrymuffine/homecaresystemapp/MainActivity.java
@@ -1,17 +1,31 @@
 package rasberrymuffine.homecaresystemapp;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     /**
      * Created by 예림 on 2015-09-09.
      */
+    VideoView videoView;
+    Button callButton;
+    Button speakButton;
+    Button logButton;
+    Button settingButton;
+    Switch doorControlSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +35,32 @@ public class MainActivity extends AppCompatActivity {
         String serialNum = fromLoginIntent.getStringExtra("serialNum");
 
         Toast.makeText(this,serialNum+"" , Toast.LENGTH_LONG).show();
-    }
 
+        videoView = (VideoView)findViewById(R.id.videoView);
+        callButton = (Button)findViewById(R.id.callButton);
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                call();
+            }
+        });
+        speakButton = (Button)findViewById(R.id.speakButton);
+        logButton = (Button)findViewById(R.id.logButton);
+        // settingButton = (Button)findViewById(R.id.settingButton);
+        doorControlSwitch = (Switch)findViewById(R.id.openSwitch);
+
+
+    }
+    private void call(){
+        String num = "01093866983";                     // 사용자가 등록한 긴급전화번호를 사용해도 좋을듯
+        try {
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);     // ACTION CALL 쓰면 곧장 걸린다고 함 _ 나는 통신이 안돼서 확인 불가 ㅜㅜ
+            callIntent.setData(Uri.parse("tel:" + num));
+            startActivity(callIntent);
+        }catch(ActivityNotFoundException e){
+            Log.e("전화를 겁니다.", "전화를 걸 수 없습니다.", e);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
