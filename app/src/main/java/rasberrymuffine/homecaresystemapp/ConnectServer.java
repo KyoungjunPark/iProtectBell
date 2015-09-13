3065a2da1aa3c27bee68fefc05cdc94fa1c010e4
PARK KYOUNGJUN <kjpark@uclab.re.kr>
2015-09-13 16:42:40 +0900

    server communication

--- app/src/main/java/rasberrymuffine/homecaresystemapp/ConnectServer.java
+++ app/src/main/java/rasberrymuffine/homecaresystemapp/ConnectServer.java
@@ -1,7 +1,45 @@
 package rasberrymuffine.homecaresystemapp;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.simple.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by USER on 2015-09-13.
  */
 public class ConnectServer {
+    public static void Get_Log() {
+        CommunicationTask task = new CommunicationTask();
+        task.execute("log");
+    }
+
+    private static class CommunicationTask extends AsyncTask<String, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            if(params[0] == "log") {
+                try {
+                    URL url = new URL("http://165.194.104.19:5000/log");
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                    String line;
+                    while ((line = rd.readLine()) != null) {
+                        Log.d("hello", line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return true;
+        }
+    }
 }
--- app/src/main/java/rasberrymuffine/homecaresystemapp/InnerDatabase.java
+++ app/src/main/java/rasberrymuffine/homecaresystemapp/InnerDatabase.java
@@ -1,8 +1,21 @@
 package rasberrymuffine.homecaresystemapp;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by 예림 on 2015-09-13.
  */
 public final class InnerDatabase {
-
+    public static final class CreateDB implements BaseColumns {
+        public static final String SOUND = "sound";
+        public static final String ALARM = "alarm";
+        public static final String REPORT_CALL = "report_call";
+        public static final String _TABLENAME = "setting";
+        public static final String _CREATE =
+                "create table "+_TABLENAME+"("
+                        +_ID+" integer primary key autoincrement, "
+                        +SOUND+" text not null , "
+                        +ALARM+" text not null , "
+                        +REPORT_CALL+" text not null );";
+    }
 }
