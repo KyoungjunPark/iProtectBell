aa51f39234fd2fc1ea952bcd28446363f3c80039
Han YeLim <dpfla0376@hanmail.net>
2015-09-15 21:20:06 +0900

    add sendToServer method in MainActivity.java

---
+++ app/src/main/java/rasberrymuffine/homecaresystemapp/FullscreenActivity.java
@@ -0,0 +1,24 @@
+package rasberrymuffine.homecaresystemapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+/**
+ * Created by 예림 on 2015-09-15.
+ */
+public class FullscreenActivity extends Activity {
+
+    WebView web;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+
+        web = (WebView) findViewById(R.id.fullScreenView);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("http://165.194.104.19:8080/stream");
+
+    }
+
+}
