445f16ee56f48017efcc36469593a3919191a10a
wonse <dldnjstp20@naver.com>
2015-09-16 15:45:54 +0900

    notification

--- app/src/main/java/rasberrymuffine/homecaresystemapp/GCMBroadcastReceiver.java
+++ app/src/main/java/rasberrymuffine/homecaresystemapp/GCMBroadcastReceiver.java
@@ -5,8 +5,12 @@ package rasberrymuffine.homecaresystemapp;
  */
 import java.net.URLDecoder;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
 
@@ -23,6 +27,7 @@ public class GCMBroadcastReceiver extends WakefulBroadcastReceiver {
             if (action.equals("com.google.android.c2dm.intent.RECEIVE")) { // 푸시 메시지 수신 시
                 String rawData = intent.getStringExtra("data");		// 서버에서 보낸 data 라는 키의 value 값
                 String data = "";
+                String title = "iProtectBell";
                 try {
                     data = URLDecoder.decode(rawData, "UTF-8");
                 } catch(Exception ex) {
@@ -30,7 +35,12 @@ public class GCMBroadcastReceiver extends WakefulBroadcastReceiver {
                 }
 
                 //Log.v(TAG,  "data : " + data);
-
+                //////////////////////////////////////////////////////////////////////////
+                Vibrator vibrator =
+                        (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+                vibrator.vibrate(1000);
+                setNotification(context, title, data);
+                //////////////////////////////////////////////////////////////////////////
                 // 액티비티로 전달
                 sendToActivity(context, data);
 
@@ -42,6 +52,24 @@ public class GCMBroadcastReceiver extends WakefulBroadcastReceiver {
         }
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////
+    private void setNotification(Context context, String title, String message) {
+        NotificationManager notificationManager = null;
+        Notification notification = null;
+        try {
+            notificationManager = (NotificationManager) context
+                    .getSystemService(Context.NOTIFICATION_SERVICE);
+            notification = new Notification(R.drawable.siren,
+                    message, System.currentTimeMillis());
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pi = PendingIntent.getActivity(context, 0, intent, 0);
+            notification.setLatestEventInfo(context, title, message, pi);
+            notificationManager.notify(0, notification);
+        } catch (Exception e) {
+
+        }
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////
     private void sendToActivity(Context context, String data) {
         Intent intent = new Intent(context, MainActivity.class);
         intent.putExtra("data", data);
@@ -49,3 +77,46 @@ public class GCMBroadcastReceiver extends WakefulBroadcastReceiver {
         context.startActivity(intent);
     }
 }
+/*
+    @Override
+    protected void onMessage(Context context, Intent intent) {
+        // 메세지를 수신했을 때 동작 설명
+        if (BuildConfig.DEBUG)
+            Log.d(LOG_TAG, "GCMReceiver Message");
+        try {
+            String title = intent.getStringExtra(PUSH_DATA_TITLE);
+            String message = intent.getStringExtra(PUSH_DATA_MESSAGE);
+            Vibrator vibrator =
+                    (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(1000);
+            setNotification(context, title, message);
+            if (BuildConfig.DEBUG)
+                Log.d(LOG_TAG, title + ":" + message);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "[onMessage] Exception : " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected boolean onRecoverableError(Context context, String errorId) {
+        if (BuildConfig.DEBUG)
+            Log.d(LOG_TAG, "onRecoverableError: " + errorId);
+        return super.onRecoverableError(context, errorId);
+    }
+
+    private void setNotification(Context context, String title, String message) {
+        NotificationManager notificationManager = null;
+        Notification notification = null;
+        try {
+            notificationManager = (NotificationManager) context
+                    .getSystemService(Context.NOTIFICATION_SERVICE);
+            notification = new Notification(R.drawable.ic_launcher,
+                    message, System.currentTimeMillis());
+            Intent intent = new Intent(context, NotificationActivity.class);
+            PendingIntent pi = PendingIntent.getActivity(context, 0, intent, 0);
+            notification.setLatestEventInfo(context, title, message, pi);
+            notificationManager.notify(0, notification);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "[setNotification] Exception : " + e.getMessage());
+        }
+    }*/
