da0cf5816b90f68cf0d286bdedb7c381ba60e84a
Han YeLim <dpfla0376@hanmail.net>
2015-09-17 22:01:28 +0900

    perfect video play~~~

--- app/src/main/java/rasberrymuffine/homecaresystemapp/FullscreenActivity.java
+++ app/src/main/java/rasberrymuffine/homecaresystemapp/FullscreenActivity.java
@@ -1,7 +1,9 @@
 package rasberrymuffine.homecaresystemapp;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -20,6 +22,15 @@ public class FullscreenActivity extends Activity {
         web.getSettings().setJavaScriptEnabled(true);
         web.loadUrl("http://165.194.104.19:8080/stream");
         web.setWebViewClient(new WebViewClient());
+        web.setInitialScale(1);
+        web.setPadding(0, 0, 0, 0);
+        web.setBackgroundColor(Color.TRANSPARENT);
+        web.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+        web.getSettings().setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        web.getSettings().setLoadWithOverviewMode(true);
+        web.getSettings().setUseWideViewPort(true);
+        web.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        web.setScrollbarFadingEnabled(false);
 
     }
 
